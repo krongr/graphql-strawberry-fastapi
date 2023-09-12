152701178f36c69714f2d99d5571c012ff592290
krongr <krongr.main@gmail.com>
2023-09-11 22:16:50 -0500

    docs: Added module docstring

--- models.py
+++ models.py
@@ -1,3 +1,36 @@
+"""
+models.py
+
+This module defines the data models `Power` and `Character` for
+the MongoDB database using the MongoEngine ODM.
+
+- `Power`:
+    Represents individual abilities or special skills.
+    Each power has a unique name and an optional description.
+    The powers are stored in the 'powers' collection.
+
+- `Character`:
+    Represents individual characters. Each character has attributes
+    like an alias, real name, role, associated powers and
+    a list of enemies. The role attribute categorizes characters into
+    predefined alignments, such as hero, villain, etc.
+    Characters are stored in the 'characters' collection and have
+    a unique combination of alias and name.
+    The model also maintains references to powers and other characters.
+
+This module plays a critical role in structuring the database schema,
+ensuring data integrity, and facilitating efficient data operations.
+
+Note:
+- When a power or character is deleted, associated references in
+  other documents are also deleted.
+- For the 'enemies' field in the Character model, LazyReferenceField is used.
+  This means when querying, only the ID of the enemy character will be
+  fetched initially. The full enemy object will not be automatically loaded
+  unless explicitly dereferenced.
+"""
+
+
 from mongoengine import (
     Document, StringField, ListField, ReferenceField, LazyReferenceField,
     CASCADE,
