04c21f97421771ec9e38ef2345955f0cb14bb3ad
krongr <krongr.main@gmail.com>
2023-09-17 17:29:51 -0500

    test: Adjusted tests.gql.resolvers.test_power_resolvers
    - Implemented previously created mock classes to standardize mock usage.
    - Fixed a typo in test_allPowers().
    - Removed error catch from test_power_invalid_id() due to uncertainty of expected behavior.

--- tests/gql/resolvers/test_power_resolvers.py
+++ tests/gql/resolvers/test_power_resolvers.py
@@ -1,14 +1,9 @@
-
-from unittest.mock import Mock
-
-import pytest
-
 from gql.types.power_types import PowerType
 from gql.resolvers.power_resolvers import PowerQuery
-from exceptions import ErrorResponse
+from tests.mock_classes import MockHandler, MockInfo, MockSelectedField
 
 
-mock_powers = {
+mock_power_types = {
     '1': PowerType(
         id='1',
         name='flight',
@@ -21,27 +16,14 @@ mock_powers = {
     ),
 }
 
-def mock_get_one(id: str) -> PowerType | None:
-    return mock_powers.get(id)
 
-def mock_get_all() -> list[PowerType]:
-    return list(mock_powers.values())
-
-@pytest.fixture
-def mock_power_handler() -> Mock:
-    handler = Mock()
-    handler.get_one.side_effect = mock_get_one
-    handler.get_all.side_effect = mock_get_all
-    return handler
-
-@pytest.fixture
-def mock_info(mock_power_handler) -> Mock:
-    mock_info = Mock()
-    mock_info.context = {'power_handler': mock_power_handler}
-    return mock_info
+mock_power_handler = MockHandler(mock_power_types)
+mock_info = MockInfo(
+    context = {'power_handler': mock_power_handler}
+)
 
 
-def test_power_valid_id(mock_info):
+def test_power_valid_id():
     result = PowerQuery().power(
         info=mock_info,
         id='1',
@@ -49,22 +31,20 @@ def test_power_valid_id(mock_info):
     assert result.name == 'flight'
     assert result.description == 'Ability to fly'
 
-def test_power_invalid_id(mock_info):
-    with pytest.raises(ErrorResponse) as error:
-        result = PowerQuery().power(
-            info=mock_info,
-            id='8',
-        )
+def test_power_invalid_id():
+    result = PowerQuery().power(
+        info=mock_info,
+        id='8',
+    )
 
-    assert error.value.msg == 'Power not found!'
-    assert error.value.code == 404
+    assert result == None
 
-def test_allPowers(mock_info):
+def test_allPowers():
     result = PowerQuery().allPowers(info=mock_info)
 
     assert type(result) == list
     assert len(result) == 2
     assert type(result[0]) == PowerType
 
-    names = [power.names for power in result]
+    names = [power.name for power in result]
     assert len(names) == len(set(names))
