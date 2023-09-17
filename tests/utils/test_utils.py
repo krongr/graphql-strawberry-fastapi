0cc341b803223f6d22140c3322a6e547f43dcf8b
krongr <krongr.main@gmail.com>
2023-09-17 17:39:29 -0500

    test: Adjusted tests.utils.test_utils
    - Implemented previously created mock classes to standardize mock usage.

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,42 +1,23 @@
-from unittest.mock import Mock
-
-import pytest
+from tests.mock_classes import MockHandler, MockInfo, MockSelectedField
 
 from utils import utils
 
 
-#  Imitates (partly) SelectedField from strawberry.types.nodes
-def mock_selected_field(
-        name: str, selections: list[Mock] = None
-    ) -> Mock:
-    selected_field = Mock()
-    selected_field.name = name
-    selected_field.selections = selections or []
-    return selected_field
-
-# Returns list of SelectedField imitations
-def mock_query_structure() -> list[Mock]:  
-    level_2 = [mock_selected_field(name) for name in 
-               ('powers', 'enemies', 'alias')]
-    level_1 = [mock_selected_field('character', level_2)]
-    return level_1
-
-# Imitates (partly) Info from strawberry.types.info
-@pytest.fixture
-def mock_info() -> Mock:
-    info = Mock()
-    info.selected_fields = mock_query_structure()
-    return info
+mock_type_fields = [MockSelectedField(name) for name in
+                    ('powers', 'enemies', 'alias')]
+mock_info = MockInfo(
+    selected_fields=[MockSelectedField('character', mock_type_fields)],
+)
 
 
-def test_get_primary_selected_fields(mock_info):
+def test_get_primary_selected_fields():
     result = utils.get_primary_selected_fields(mock_info)
 
     assert isinstance(result, list)
     assert len(result) == 3
     assert hasattr(result[0], 'selections')
 
-def test_get_selected_complex_fields(mock_info):
+def test_get_selected_complex_fields():
     selection = utils.get_primary_selected_fields(mock_info)
     result = utils.get_selected_complex_fields(selection)
 
