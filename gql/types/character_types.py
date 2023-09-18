a2cfea95ab80644adc6d639547080d1f99954969
krongr <krongr.main@gmail.com>
2023-09-18 18:01:06 -0500

    add: gql.types.character_types
    Module contains:
    - Main gql type for Character object.
    - Enum object for character roles.

---
+++ gql/types/character_types.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+import strawberry
+
+from gql.types.common_types import GQLType
+from gql.types.power_types import PowerType
+from settings import MAX_QUERY_DEPTH
+
+
+@strawberry.enum(description='Represents the alignment of a character.')
+class RoleEnum(Enum):
+    HERO = 'hero'
+    VILLIAN = 'villain'
+    ANTIHERO = 'antihero'
+
+@strawberry.type
+class CharacterType(GQLType):
+    id: strawberry.ID
+    alias: str = strawberry.field(
+        description='Character alias (e.g. Batman, Joker).'
+    )
+    name: str = strawberry.field(description='Character real name.')
+    role: RoleEnum = strawberry.field(description='Character role.')
+    powers: list[PowerType] = strawberry.field(
+        description='List of character powers.'
+    )
+    enemies: list['CharacterType'] = (
+        strawberry.field(
+            description=(
+                'List of character enemies. Will be empty, if query '
+                f'exceeds a depth level of {MAX_QUERY_DEPTH}.'
+            )
+        )
+    )
+    enemy_ids: list[strawberry.ID] = strawberry.field(
+        description='List of character enemy IDs.'
+    )
