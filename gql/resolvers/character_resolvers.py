04e88e6f830855dbaea9638f32e5593649f74b74
krongr <krongr.main@gmail.com>
2023-09-17 22:50:27 -0500

    feat: Adjusted CharacterQuery from gql.resolvers.character_resolvers
    - Methods now pass down additional data to address increased complexity of processes.

--- gql/resolvers/character_resolvers.py
+++ gql/resolvers/character_resolvers.py
@@ -4,24 +4,24 @@ import strawberry
 from strawberry.types.info import Info
 
 from gql.types.character_types import CharacterType
-from exceptions import ErrorResponse
+from utils import utils
 
 
 @strawberry.type
 class CharacterQuery:
 
     @strawberry.field
-    def character(self, info: Info, id: strawberry.ID) -> CharacterType:
+    def character(self, info: Info, id: strawberry.ID) -> Optional[CharacterType]:
+        selected_fields = utils.get_primary_selected_fields(info)
         handler = info.context['character_handler']
-        character = handler.get_one(id)
+        character = handler.get_one_by_id(id, selected_fields)
 
-        if not character:
-            raise ErrorResponse('Character not found!', 404)
         return character
 
     @strawberry.field
     def allCharacters(self, info) -> list[CharacterType]:
+        selected_fields = utils.get_primary_selected_fields(info)
         handler = info.context['character_handler']
-        all_characters = handler.get_all()
+        all_characters = handler.get_all(selected_fields)
 
         return all_characters
