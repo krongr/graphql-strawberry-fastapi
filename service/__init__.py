3c2965603db823beb98627651dceb2761be7cd9f
krongr <krongr.main@gmail.com>
2023-09-17 22:44:58 -0500

    add: service package
    - This package will contain handlers that will be middleware between resolvers and DAO.
    - Added get_character_handler() and get_power_handler() to __init__.py module for easier access.

---
+++ service/__init__.py
@@ -0,0 +1,9 @@
+from service.character_handler import CharacterHandler
+from service.power_handler import PowerHandler
+
+
+def get_character_handler():
+    return CharacterHandler
+
+def get_power_handler():
+    return PowerHandler
