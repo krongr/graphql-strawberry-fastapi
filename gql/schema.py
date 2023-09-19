6958ae3c75a33a19fc677dae66824ba412979dcd
krongr <krongr.main@gmail.com>
2023-09-19 15:57:44 -0500

    feat: Added all resolvers to gql schema
    - CharacterQuery from gql.resolvers.character_resolvers
    - PowerQuery from gql.resolvers.power_resolvers

--- gql/schema.py
+++ gql/schema.py
@@ -1,20 +1,26 @@
 import strawberry
 from strawberry.fastapi import GraphQLRouter
+from strawberry.tools import merge_types
 
 import service
+from gql.resolvers.character_resolvers import CharacterQuery
+from gql.resolvers.power_resolvers import PowerQuery
 
 
 @strawberry.type
-class Query:
+class TestQuery:
     @strawberry.field
-    def hello(self, name: str = "World") -> str:
-        return f"Hello {name}!"
+    def hello(self, name: str = 'World') -> str:
+        return f'Hello {name}!'
+    
+
+queries = merge_types('Query', (TestQuery, CharacterQuery, PowerQuery))
 
 
 gql_router = GraphQLRouter(
-    schema=strawberry.Schema(query=Query),
+    schema=strawberry.Schema(query=queries),
     context_getter=lambda: {
-            "character_handler": service.get_character_handler(),
-            "power_handler": service.get_power_handler()
+            'character_handler': service.get_character_handler(),
+            'power_handler': service.get_power_handler()
         }
 )
