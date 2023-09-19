7af8573f41adf6426539d2caf14a8bd493b05ce4
krongr <krongr.main@gmail.com>
2023-09-18 21:43:13 -0500

    feat: Reintroduced data_access package DAO modules
    - Implemented BaseDAO class inside base_dao module. This class encapsulates all generic CRUD operations.
    - CharacterDAO and PowerDAO from relevant modules will provide type hinting and custom methods if needed.

---
+++ data_access/base_dao.py
@@ -0,0 +1,47 @@
+from typing import Generic, TypeVar
+
+from mongoengine.errors import MongoEngineException
+
+from models import Character, Power
+from logger import CustomLogger
+
+
+T = TypeVar('T', Character, Power)
+logger = CustomLogger('data_access.character_dao')
+
+
+class BaseDAO(Generic[T]):
+    model = None
+
+    @classmethod
+    def get_one_by_id(cls, id: str) -> T | None:
+        if not cls.model:
+            raise ValueError("Model not set for this DAO.")
+
+        try:
+            return cls.model.objects(id=id).first()
+        except MongoEngineException:
+            logger.log_error("DB interaction error")
+            raise
+
+    @classmethod
+    def get_many_by_ids(cls, ids: list[str]) -> list[T]:
+        if not cls.model:
+            raise ValueError("Model not set for this DAO.")
+
+        try:
+            return list(cls.model.objects(id__in=ids))
+        except MongoEngineException:
+            logger.log_error("DB interaction error")
+            raise
+
+    @classmethod
+    def get_all(cls) -> list[T]:
+        if not cls.model:
+            raise ValueError("Model not set for this DAO.")
+        
+        try:
+            return list(cls.model.objects())
+        except MongoEngineException:
+            logger.log_error("DB interaction error")
+            raise
