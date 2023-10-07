00271675aeef1331e928f310c73b4ea9e1852bf1
krongr <krongr.main@gmail.com>
2023-10-07 18:50:28 -0500

    doc: Added docstrings and inline comments for main

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+"""
+main.py
+
+This module initializes a FastAPI application with a MongoDB backend
+and sets up routing for GraphQL operations. It provides a health check
+endpoint to verify that the service is operational.
+
+Usage:
+    Run the script directly to start the FastAPI server:
+    $ python main.py
+
+    Or run uvicorn from terminal:
+    $ uvicorn main:app --host 0.0.0.0 --port 80
+"""
+
+
 import mongoengine
 from fastapi import FastAPI
 
@@ -5,14 +21,19 @@ from gql.schema import gql_router
 from settings import MONGODB_CONNECTION
 
 
+# Initializing MongoDB connection for the app.
 mongoengine.connect(**MONGODB_CONNECTION)
 
 app = FastAPI()
 
 @app.get('/')
 def health_check():
+    """
+    Check the health status of the service.
+    """
     return {'status': 'ok'}
 
+# Including the GraphQL router to the FastAPI app.
 app.include_router(gql_router, prefix="/graphql")
 
 
