6ab4a35acd7b92f31fabd673c1b0f36c6bbbb69c
krongr <krongr.main@gmail.com>
2023-10-07 18:30:06 -0500

    doc: Added docstrings and inline comments to gql.schema

--- gql/schema.py
+++ gql/schema.py
@@ -1,3 +1,19 @@
+"""
+schema.py
+
+This module sets up the GraphQL router for the application.
+It merges different GraphQL query resolvers and provides a
+unified schema for the app.
+The router is configured with context getters to fetch the necessary
+services for resolving GraphQL queries.
+
+Classes:
+    - TestQuery: A simple GraphQL query for demonstration purposes.
+
+
+"""
+
+
 import strawberry
 from strawberry.fastapi import GraphQLRouter
 from strawberry.tools import merge_types
@@ -9,16 +25,25 @@ from gql.resolvers.power_resolvers import PowerQuery
 
 @strawberry.type
 class TestQuery:
+    """
+    A simple GraphQL query mostly for health checks.
+    Can be omited or removed in future versions.
+    """
     @strawberry.field
     def hello(self, name: str = 'World') -> str:
         return f'Hello {name}!'
     
 
+# Merging individual GraphQL query, mutation and subscription resolvers
+# to create a unified set.
 queries = merge_types('Query', (TestQuery, CharacterQuery, PowerQuery))
 
 
+# Setting up the GraphQL router with the merged queries and
+# configuring context to provide necessary handlers.
 gql_router = GraphQLRouter(
     schema=strawberry.Schema(query=queries),
+    # Providing the necessary handlers as context for GraphQL resolvers.
     context_getter=lambda: {
             'character_handler': service.get_character_handler(),
             'power_handler': service.get_power_handler()
