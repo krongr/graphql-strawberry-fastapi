cd3859a25c3aebe7f4e741c7c6fe60e197acabe3
krongr <krongr.main@gmail.com>
2023-10-07 18:52:26 -0500

    enhancement: Cosmetic fix in exceptions

--- exceptions.py
+++ exceptions.py
@@ -3,5 +3,5 @@
 class FileError(Exception):
     """Raised for errors related to file operations."""
 
-    def __init__(self, fpath:str):
+    def __init__(self, fpath: str):
         super().__init__(f'file path: {fpath}')
