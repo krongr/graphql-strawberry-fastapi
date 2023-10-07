bb46139b122182b96b86088e3824832e8b604d8c
krongr <krongr.main@gmail.com>
2023-10-07 16:28:23 -0500

    feat: Added error handling and docstrings to gql.resolvers.power_resolvers

--- gql/resolvers/power_resolvers.py
+++ gql/resolvers/power_resolvers.py
@@ -1,9 +1,23 @@
+"""
+power_resolvers.py
+
+This module provides the necessary GraphQL queries related to
+the Power domain. The actual data processing is delegated to the
+'power_handler' which is expected to be provided in
+the GraphQL context. 
+"""
+
+
 from typing import Optional
 
 import strawberry
 from strawberry.types.info import Info
 
 from gql.types.power_types import PowerType
+from logger import CustomLogger
+
+
+logger = CustomLogger('service.power_resolvers')
 
 
 @strawberry.type
@@ -11,14 +25,40 @@ class PowerQuery:
 
     @strawberry.field
     def power(self, info: Info, id: strawberry.ID) -> Optional[PowerType]:
-        handler = info.context['power_handler']
-        power = handler.get_one_by_id(id)
+        """
+        Fetches a single Power entity based on provided ID.
 
+        :param info: GraphQL context. 
+        :param id: ObjectID of a power document in MongoDB.
+
+        :return: PowerType or None if there is no document
+                 with provided ID.
+        """
+        try:
+            handler = info.context['power_handler']
+        except (KeyError, AttributeError, TypeError):
+            logger.log_error('Failed to access handler or it was not provided')
+            return None
+
+        power = handler.get_one_by_id(id)
         return power
 
     @strawberry.field
     def allPowers(self, info: Info) -> list[PowerType]:
-        handler = info.context['power_handler']
-        all_powers = handler.get_all()
+        """
+        Fetches all Power entities.
 
+        :param info: GraphQL context. 
+
+        :return: List of PowerTypes. List will be empty if
+                 there are no power documents or failed to
+                 access handler.
+        """
+        try:
+            handler = info.context['power_handler']
+        except (KeyError, AttributeError, TypeError):
+            logger.log_error('Failed to access handler or it was not provided')
+            return []
+
+        all_powers = handler.get_all()
         return all_powers
